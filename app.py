7bd2b49e1615dc7515b09408c03667ef50158db9
Mfadel-ux <fadelmuhammad5335@gmail.com>
2026-06-04 16:27:51 +0800

    app.py

--- app.py
+++ app.py
@@ -1,25 +1,5 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-import sklearn
-import joblib
-import sys
 
-st.title("Burnout Test")
+st.title("Deployment Test")
 
-st.write("Python:", sys.version)
-st.write("Sklearn:", sklearn.__version__)
-st.write("Pandas:", pd.__version__)
-st.write("Numpy:", np.__version__)
-st.write("Joblib:", joblib.__version__)
-
-try:
-    model = joblib.load("burnout_predictor.pkl")
-
-    st.success("Model Loaded")
-    st.write(type(model))
-
-except Exception as e:
-
-    st.error("Load Failed")
-    st.exception(e)
+st.success("App is running")
